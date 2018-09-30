f1918ea35444d9762f1c9e7d28f1a91e99cc2f66
Joe le taxi <julien.vera@hotmail.com>
2018-09-30 16:00:32 +0200

    Refactoring and cleansing of FE. Add register button - need BE implementation.

--- app/src/main/java/com/jvera/chat_app/MainActivity.java
+++ app/src/main/java/com/jvera/chat_app/MainActivity.java
@@ -1,7 +1,8 @@
 package com.jvera.chat_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +17,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     final static private String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.login) EditText login;
-    @BindView(R.id.password) EditText password;
-    @BindView(R.id.check_login) Button button;
+    @BindView(R.id.password) TextInputLayout password;
+    @BindView(R.id.check_login_btn) Button check_login_btn;
+    @BindView(R.id.register_btn) Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +27,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate");
         ButterKnife.bind(this);
-        login.setBackgroundColor(getResources().getColor(R.color.something));
-        button.setOnClickListener(this);
-        password.setOnClickListener(this);
+//        login.setBackgroundColor(getResources().getColor(R.color.some_red));
+
+        check_login_btn.setOnClickListener(this);
+        register_btn.setOnClickListener(this);
     }
 
     @Override public void onClick(View v) {
         String login_value = login.getText().toString();
-        String password_value = password.getText().toString();
+        String password_value = password.getEditText().getText().toString(); //IDE whining for no damn reason
 
         switch(v.getId()) {
-            case R.id.check_login:
+            case R.id.check_login_btn:
                 if(login_value.isEmpty() || password_value.isEmpty()){
                     TextView Error_pop = findViewById(R.id.Error_pop);
                     Error_pop.setVisibility(View.VISIBLE);
                     break;
                 }
                 Toast.makeText(
-                    MainActivity.this,
-                    "Hi " + login_value + "sweet password " + password_value,
-                    Toast.LENGTH_SHORT
+                    MainActivity.this, "Hi " + login_value, Toast.LENGTH_SHORT
                 ).show();
+                //
+                //TODO: something
+                //
                 break;
+
+            case R.id.register_btn:
+                //
+                //TODO: register logic
+                //
+                break;
+
             default:
                 //should never get here
                 break;
